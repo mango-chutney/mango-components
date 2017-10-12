76cddf85b9a61765475eb70caa6a03d169e57a9b
Ruben Maher <r@rkm.id.au>
2017-10-12 11:46:59 +1030

    Fix build

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BABEL=${BABEL:-"$(npm bin)/babel"}
+EXTRA_NODE_ARGS=${EXTRA_NODE_ARGS:-""}
+NODE=${NODE:-"${npm_node_execpath}"}
+NPM_RUN_ALL=${NPM_RUN_ALL:-"$(npm bin)/npm-run-all"}
+
+eval "${NODE}" "${EXTRA_NODE_ARGS[@]}" "${BABEL}" "." \
+     --ignore "examples,flow-typed,lib,node_modules,package.json,scripts,webpack.config.js,yarn.*" \
+     --copy-files \
+     --quiet \
+     --out-dir "./lib"
