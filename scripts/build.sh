55831ed4024bec175391a1b0336c422faf4f1f78
Ruben Maher <r@rkm.id.au>
2017-10-12 11:55:14 +1030

    No really

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ NODE=${NODE:-"${npm_node_execpath}"}
 NPM_RUN_ALL=${NPM_RUN_ALL:-"$(npm bin)/npm-run-all"}
 
 eval "${NODE}" "${EXTRA_NODE_ARGS[@]}" "${BABEL}" "." \
-     --ignore "examples,flow-typed,lib,node_modules,package.json,scripts,webpack.config.js,yarn.*" \
+     --ignore "examples,flow-typed,node_modules,package.json,scripts,webpack.config.js,yarn.*" \
      --copy-files \
      --quiet \
      --out-dir "./lib"
