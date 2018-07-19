542ec6af2db4d76f19a6f3c9f5fb3e045a326bd0
Ruben Maher <ruben@maher.fyi>
2018-07-19 12:25:26 +0930

    Improve prettier script

--- scripts/prettier.sh
+++ scripts/prettier.sh
@@ -2,18 +2,15 @@
 
 set -euo pipefail
 
-EXTRA_NODE_ARGS=${EXTRA_NODE_ARGS:-""}
-NODE=${NODE:-"${npm_node_execpath}"}
-PRETTIER=${PRETTIER:-"$(npm bin)/prettier"}
-
-eval "${NODE}" "${EXTRA_NODE_ARGS[@]}" "${PRETTIER}" \
-     --write \
-     "./src/components/**/*.{js,json}" \
-     "./examples/components/**/*.{js,json}" \
-     "./examples/containers/**/*.{js,json}" \
-     "./examples/reducers/**/*.{js,json}" \
-     "./examples/store/**/*.{js,json}" \
-     "./examples/*.{js,json}" \
-     ".eslintrc.json" \
-     ".package.json" \
-     "README.md"
+find . \
+     \( \
+     -name '*.js' \
+     -o -name '*.babelrc' \
+     -o -name '*.json' \
+     -o -name '*.md' \
+     -o -name '*.html' \
+     \) \
+     -not -path './node_modules/*' \
+     -not -path './flow-typed/*' \
+     -not -path './lib/*' \
+     -print0 | xargs --null "$(npm bin)/prettier" --write
