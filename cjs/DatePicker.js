42c2c0ec99f4616ed37bf6de28afc63472171a91
Jason Di Benedetto <jason.dibenedetto6@gmail.com>
2018-07-31 11:29:49 +0930

    pass ref to date picker input component

--- cjs/DatePicker.js
+++ cjs/DatePicker.js
@@ -5,6 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = exports.createComponent = exports.createStyledComponents = exports.defaultStyleProps = void 0;
 
+require("core-js/modules/es7.symbol.async-iterator");
+
+require("core-js/modules/es6.symbol");
+
 require("core-js/modules/es6.object.assign");
 
 require("core-js/modules/web.dom.iterable");
@@ -35,11 +39,29 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
 function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
@@ -101,45 +123,85 @@ var createStyledComponents = function createStyledComponents(styleProps) {
 
 exports.createStyledComponents = createStyledComponents;
 
-function DatePicker(_ref) {
-  var InputComponent = _ref.InputComponent,
-      LabelComponent = _ref.LabelComponent,
-      OverlayWrapperComponent = _ref.OverlayWrapperComponent,
-      OverlayComponent = _ref.OverlayComponent,
-      input = _ref.input,
-      label = _ref.label,
-      dateFormat = _ref.dateFormat,
-      inputProps = _ref.inputProps,
-      calendarProps = _ref.calendarProps,
-      rest = _objectWithoutProperties(_ref, ["InputComponent", "LabelComponent", "OverlayWrapperComponent", "OverlayComponent", "input", "label", "dateFormat", "inputProps", "calendarProps"]);
+var DatePicker =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(DatePicker, _React$Component);
 
-  return React.createElement("label", {
-    htmlFor: input.name
-  }, label && React.createElement(LabelComponent, null, label), React.createElement(_DayPickerInput.default, _extends({
-    format: dateFormat,
-    formatDate: formatDate,
-    parseDate: parseDate,
-    placeholder: "".concat(_luxon.DateTime.local().toFormat(dateFormat)),
-    inputProps: _objectSpread({}, input),
-    component: function component(props) {
-      return React.createElement(InputComponent, props);
-    },
-    onDayChange: function onDayChange(day) {
-      return input.onChange(formatDate(day, dateFormat));
-    },
-    overlayComponent: function overlayComponent(_ref2) {
-      var children = _ref2.children,
-          props = _objectWithoutProperties(_ref2, ["children"]);
+  function DatePicker() {
+    var _getPrototypeOf2;
 
-      return React.createElement(OverlayWrapperComponent, props, React.createElement(OverlayComponent, null, children));
-    },
-    dayPickerProps: _objectSpread({}, calendarProps)
-  }, rest)));
-}
+    var _this;
 
-DatePicker.defaultProps = {
+    _classCallCheck(this, DatePicker);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(DatePicker)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "inputRef", React.createRef());
+
+    return _this;
+  }
+
+  _createClass(DatePicker, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      if (this.inputRef.current) {
+        this.inputRef.current.focus();
+      }
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      var _this$props = this.props,
+          InputComponent = _this$props.InputComponent,
+          LabelComponent = _this$props.LabelComponent,
+          OverlayWrapperComponent = _this$props.OverlayWrapperComponent,
+          OverlayComponent = _this$props.OverlayComponent,
+          input = _this$props.input,
+          label = _this$props.label,
+          dateFormat = _this$props.dateFormat,
+          calendarProps = _this$props.calendarProps,
+          rest = _objectWithoutProperties(_this$props, ["InputComponent", "LabelComponent", "OverlayWrapperComponent", "OverlayComponent", "input", "label", "dateFormat", "calendarProps"]);
+
+      return React.createElement("label", {
+        htmlFor: input.name
+      }, label && React.createElement(LabelComponent, null, label), React.createElement(_DayPickerInput.default, _extends({
+        format: dateFormat,
+        formatDate: formatDate,
+        parseDate: parseDate,
+        placeholder: "".concat(_luxon.DateTime.local().toFormat(dateFormat)),
+        inputProps: _objectSpread({}, input),
+        component: function component(inputProps) {
+          return React.createElement(InputComponent, _extends({}, inputProps, {
+            innerRef: _this2.inputRef
+          }));
+        },
+        onDayChange: function onDayChange(day) {
+          return input.onChange(formatDate(day, dateFormat));
+        },
+        overlayComponent: function overlayComponent(_ref) {
+          var children = _ref.children,
+              overlayProps = _objectWithoutProperties(_ref, ["children"]);
+
+          return React.createElement(OverlayWrapperComponent, overlayProps, React.createElement(OverlayComponent, null, children));
+        },
+        dayPickerProps: _objectSpread({}, calendarProps)
+      }, rest)));
+    }
+  }]);
+
+  return DatePicker;
+}(React.Component);
+
+_defineProperty(DatePicker, "defaultProps", {
   dateFormat: 'dd/LL/yyyy'
-};
+});
 
 var createComponent = function createComponent() {
   var defaultStyledComponents = createStyledComponents(defaultStyleProps);
