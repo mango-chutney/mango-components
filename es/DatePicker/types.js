cc985d3648bfdc959765cc25ace7db6fc69c4000
Ruben Maher <ruben@maher.fyi>
2018-08-02 11:26:54 +0930

    Compose DatePicker with Input component properly

---
+++ es/DatePicker/types.js
@@ -0,0 +1,5 @@
+// Adapted from DayPickerInput.propTypes here:
+// https://github.com/gpbl/react-day-picker/blob/bb771db4da61c0d263b2deb4722bc5e8ddfacf50/src/DayPickerInput.js#L90-L123
+// And TS types here:
+// https://github.com/gpbl/react-day-picker/tree/3577cdfe9039b0ce1b47b2511ffb76f2c9c4308d/types
+import * as React from 'react';
