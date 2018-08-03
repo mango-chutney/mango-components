146e93b15fee6fe1be20dd9644ea8b318b9b6b66
Ruben Maher <ruben@maher.fyi>
2018-08-03 12:35:58 +0930

    Add the most important global styles

--- examples/containers/App.js
+++ examples/containers/App.js
@@ -3,9 +3,28 @@ import { Provider } from 'react-redux';
 import { withRouter } from 'react-router';
 import { renderRoutes } from 'react-router-config';
 import { HashRouter as Router, Switch } from 'react-router-dom';
+import styled, { injectGlobal } from 'styled-components';
 import routes from '../routes';
 import { withFooter } from '../components/Footer';
 import { withNavigation } from '../components/Navigation';
+
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  html {
+    box-sizing: border-box;
+    font-size: 100%;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+`;
+
+const Wrapper = styled.div`
+  margin: 1rem;
+`;
 
 const App = withRouter(
   withNavigation(
@@ -16,7 +35,9 @@ const App = withRouter(
 export default ({ store }) => (
   <Router>
     <Provider {...{ store }}>
-      <App />
+      <Wrapper>
+        <App />
+      </Wrapper>
     </Provider>
   </Router>
 );
