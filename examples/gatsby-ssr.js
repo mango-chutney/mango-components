c2bd54589335a4cd05dfffcd8c94b7e0fda90f33
Ruben Maher <ruben@maher.fyi>
2018-08-13 12:17:34 +0930

    gatsby-ssr: fix styled-components
    The replaceRenderer doesn't get composed.  If you implement it in your site's
    gastby-ssr file and also use a plugin that implements it, you have to compose
    them yourself.  In this case, it's gatsby-plugin-styled-components.

--- examples/gatsby-ssr.js
+++ examples/gatsby-ssr.js
@@ -3,16 +3,27 @@
 import * as React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 import configureStore from './src/store/configureStore';
 
 // eslint-disable-next-line import/prefer-default-export
 export const replaceRenderer = ({
   bodyComponent,
   replaceBodyHTMLString,
+  setHeadComponents,
 }: any) => {
   const store = configureStore();
+  const sheet = new ServerStyleSheet();
 
   replaceBodyHTMLString(
-    renderToString(<Provider {...{ store }}>{bodyComponent}</Provider>),
+    renderToString(
+      <Provider {...{ store }}>
+        <StyleSheetManager sheet={sheet.instance}>
+          {bodyComponent}
+        </StyleSheetManager>
+      </Provider>,
+    ),
   );
+
+  setHeadComponents([sheet.getStyleElement()]);
 };
