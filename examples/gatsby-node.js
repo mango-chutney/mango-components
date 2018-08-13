fe8849b95c19d6002f0bf07b3f7be81e58302494
Ruben Maher <ruben@maher.fyi>
2018-08-13 11:46:59 +0930

    ci: ignore gh-pages branch

--- examples/gatsby-node.js
+++ examples/gatsby-node.js
@@ -3,6 +3,7 @@
 /* eslint-env node */
 
 const systemPath = require('path');
+const { copy: copyFile } = require('fs-extra');
 const { peerDependencies } = require('../package.json');
 
 exports.onCreateWebpackConfig = ({ actions } /* : any */) => {
@@ -19,3 +20,13 @@ exports.onCreateWebpackConfig = ({ actions } /* : any */) => {
     },
   });
 };
+
+// Copy CircleCI configuration to deploy dir so that it ends up in the gh-pages
+// branch.  Otherwise, pushes to that branch won't be ignored by CircleCI.  Note
+// that you need to run `gh-pages` with the --dotfiles flag for this to be
+// copied.
+exports.onPostBuild = () =>
+  copyFile(
+    systemPath.resolve(systemPath.join(__dirname, '..', '.circleci')),
+    systemPath.resolve(systemPath.join(__dirname, 'public', '.circleci')),
+  );
